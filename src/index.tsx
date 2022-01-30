0726052c07644ebf8859509c59d266815bbf6fbf
souenxx <souen0823@gmail.com>
2022-01-30 22:15:03 +0900

    chore: set TouchBackend in DndProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import { render } from "react-dom";
 import Example from "./example";
 import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-
+// import { HTML5Backend } from "react-dnd-html5-backend";
+import { SampleDragLayer } from "./SampleDragLayer";
+import { TouchBackend } from "react-dnd-touch-backend";
 function App() {
   return (
     <div className="App">
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
         <Example />
+        <SampleDragLayer />
       </DndProvider>
     </div>
   );
